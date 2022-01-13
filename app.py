3a2225a4e9ae1e260c56882719c0e6c53dc1885e
onticinc <onticinc@gmail.com>
2022-01-13 16:09:18 -0700

    car

---
+++ app.py
@@ -0,0 +1,50 @@
+    # Comment
+    # Todo build this function
+    
+
+# name = "Johnny"
+# print(name)
+
+# nothing = None
+# is_working = True
+# car_off = False
+
+# if is_working:
+#     print('this is true')
+# else:
+#     print('this is not true')
+
+
+# print(15 / 6)
+# print(15 // 6)
+
+# statement = "my code rulez" [3:7]
+# print(statement)
+
+# date = "today"
+# food = "oatmeal"
+# meals = "breakfast"
+
+# mymessage = f"{date} i ate {food} for {meals}."
+
+# print(mymessage)
+
+def say_hello(friend="Tim"):
+  print("Hello, {}!".format(friend))
+
+def move(name, city="Seattle", state="Washington"):
+  msg = "{} is moving to {}, {}"
+  msg = msg.format(name, city, state)
+  print(msg)
+
+move("Charlie")
+
+say_hello('mike')
+
+def car(name, type='honda')
+    msg ="{} is a {} "
+    msg = msg.format(name, type)
+    print(msg)
+
+car('civic')
+
