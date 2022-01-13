16a9a7fbb72ca1ab95a0f3646069560eb6d4decf
onticinc <onticinc@gmail.com>
2022-01-13 16:25:57 -0700

    names

--- app.py
+++ app.py
@@ -29,6 +29,14 @@
 
 # print(mymessage)
 
+
+
+
+
+
+from unittest import result
+
+
 def say_hello(friend="Tim"):
   print("Hello, {}!".format(friend))
 
@@ -41,10 +49,49 @@ move("Charlie")
 
 say_hello('mike')
 
-def car(name, type='honda')
+def car(name, type='honda'):
     msg ="{} is a {} "
     msg = msg.format(name, type)
     print(msg)
 
 car('civic')
 
+students = [
+    { 
+        "name": "Kimmie",
+        "city": "Atlanta"
+    },
+    { 
+        "name": "Chris",
+        "city": "Salt Lake City"
+    },
+    { 
+        "name": "Zack",
+        "city": "Los Angeles"
+    }
+]
+
+
+def get_cities(students):
+    '''Return a [list] of all cities'''
+
+    result = []
+
+    for s in students:
+        if s.get('city'):
+            result.append(s.get('city'))
+    return result
+
+print('cities list:', get_cities(students))
+
+
+def get_names(students):
+    
+    result = []
+
+    for n in students:
+        if n.get('name'):
+            result.append(n.get('name'))
+    return result
+
+print('name list:', get_names(students))
