8642fbca07ed05c1811c0ca24815913116473584
onticinc <onticinc@gmail.com>
2022-01-13 18:47:35 -0700

    almost done

--- deliverable.py
+++ deliverable.py
@@ -41,3 +41,66 @@ else:
     print('short string')
 if (len(str[0]) == 'p'):
     print('starts with p')
+
+#7
+num = 15
+if num % 3 == 0:
+    print('divisible by 3')
+elif num % 5 == 0: 
+    print('divisible by 5')
+
+#8
+
+if num % 3 == 0:
+    print('divisible by 3')
+if num % 5 == 0:
+    print('divisible by 5')
+
+#9
+str = 'General Assembly'
+if str[0] == str[0].upper():
+    print('starts with a capital')
+if len(str[-1]) == len(str[-1]):
+    print('ends with a capital')
+
+#10
+num = -44
+if num > 0:
+    print('positive')
+elif num < 0:
+    print('negative')
+else:
+    print(0)
+if num % 2 == 0:
+    print('even')
+else:
+    print('odd')
+
+#11
+num = 11
+if num > 5:
+    print('less than "if"')
+
+if num > 5:
+    print('if')
+else:
+    print('else')
+
+#Task 5 
+#12
+def sayHello(name):
+    msg = 'Hello, ' + name + '. How are you?'
+    return msg
+print(sayHello('mike'))
+
+#13
+def checkNumber(num):
+    if num > 10:
+        return 'positive'
+    elif (num < 0):
+        return 'negative'
+    else:
+        return 'zero'
+    
+
+print(checkNumber(5))
